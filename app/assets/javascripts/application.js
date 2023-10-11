87f5d88ebe7635689121788d1494446aba2e1ad2
benraine <benraine@users.noreply.github.com>
2023-10-11 14:47:11 +0100

    Initial commit
    GitOrigin-RevId: 93663be6e8d5bdad946673914424111dfe4938ce

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,52 +1,8 @@
-/* global $ */
-
 //
 // For guidance on how to add JavaScript see:
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-
-$(document).ready(function () {
-
-// Add number input polyfill
-// For options and data attributes, see number-input.js
-// moved this inside function
-  window.GOVUK.numberInput.init();
-
-  if (document.getElementById('location-autocomplete')) {
-    openregisterLocationPicker({
-      selectElement: document.getElementById('location-autocomplete')
-    })
-  }
+window.GOVUKPrototypeKit.documentReady(() => {
+  // Add JavaScript here
 })
-
-function nextPageBasedOnSelection($radioObject) {
-  if (nextPageRoutes) {
-    for (let index = 0; index < Object.keys(nextPageRoutes).length; index++) {
-      var checkboxValue = $radioObject.val()
-      if (nextPageRoutes[checkboxValue] != undefined) {
-        var nextPageField = null
-        if ($('#next-page').length) {
-          nextPageField = $('#next-page')
-        } else {
-          nextPageField = $('<input type="hidden" id="next-page" name="next-page">')
-          $('form').append(nextPageField)
-        }
-        nextPageField.val(nextPageRoutes[checkboxValue])
-        console.log('Next page destination has been set as "' + nextPageRoutes[checkboxValue] + '"')
-        break
-      } else {
-        $('#next-page').remove()
-      }
-    }
-  }
-}
-    
-  $('input[type=radio]').on('change', function() {
-    try { 
-      $('input[type=radio]:checked').each(function() {
-        nextPageBasedOnSelection($(this))
-      })
-    } catch (e) {}
-  }) 
-
--- app/filters.js
+++ app/filters.js
@@ -1,19 +1,10 @@
+//
+// For guidance on how to create filters see:
+// https://prototype-kit.service.gov.uk/docs/filters
+//
+
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
-const filters = {}
+// Add your filters here
 
-/* ------------------------------------------------------------------
-    keep the following line to return your filters to the app
-  ------------------------------------------------------------------ */
-
-filters.formatMonth = function (monthAsNumber) {
-  const months = [
-    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-    'September', 'October', 'November', 'December'
-  ]
-  return months[monthAsNumber - 1]
-}
-
-// Add the filters using the addFilter function
-Object.entries(filters).forEach(([name, fn]) => addFilter(name, fn))
