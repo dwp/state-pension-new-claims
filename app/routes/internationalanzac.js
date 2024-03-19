6a7881c0adb948b6daa161aa0a41913989aa5e78
Kit-Rosenberg <148774030+Kit-Rosenberg@users.noreply.github.com>
2024-03-19 11:28:08 +0000

    anzac current created
    GitOrigin-RevId: e1de4f9bc4dbd38fda7d94ab356e6856d5983f16

--- app/routes/internationalanzac.js
+++ app/routes/internationalanzac.js
@@ -86,3 +86,61 @@ router.post('/pension-credit/anzac/pension-credit-decision-1', function (req, re
 })
 
 module.exports = router
+
+// CFN1282-current
+
+router.post('/international-task/anzac-cfn1282-current/cfn-task', function (req, res) {
+  if (req.body['cfn1282-returned'] === 'yes') {
+    res.redirect('date-of-entry')
+  } else {
+    res.redirect('get-a-task-2')
+  }
+})
+
+router.post('/international-task/anzac-cfn1282-current/date-of-entry', function (req, res) {
+  if (req.body['date-of-entry'] === 'before') {
+    res.redirect('rf1-send')
+  } else {
+    res.redirect('check-information')
+  }
+})
+
+router.post('/international-task/anzac-cfn1282-current/check-information', function (req, res) {
+  if (req.body['calculateNew'] === 'yes') {
+    res.redirect('calculate-anzac')
+  } else {
+    res.redirect('get-a-task-2')
+  }
+})
+
+router.post('/international-task/anzac-cfn1282-current/rf1-send', function (req, res) {
+  if (req.body.rf1 === 'request') {
+    res.redirect('rf1-hmrc')
+  } else if (req.body.rf1 === 'received') {
+    res.redirect('check-information')
+  } else {
+    res.redirect('get-a-task-2')
+  }
+})
+
+router.post('/international-task/anzac-cfn1282-current/sp-increase', function (req, res) {
+  if (req.body['sp-increase'] === 'yes') {
+    res.redirect('review-award')
+  } else {
+    res.redirect('get-a-task-2')
+  }
+})
+
+router.post('/international-task/anzac-cfn1282-current/calculate-anzac-routing', function (req, res) {
+  res.redirect('check-award-anzac')
+})
+
+router.post('/international-task/anzac-cfn1282-current/rf1-task', function (req, res) {
+  if (req.body['rf1-returned'] === 'yes') {
+    res.redirect('sp-increase')
+  } else {
+    res.redirect('rf1-hmrc')
+  }
+})
+
+
