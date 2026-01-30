e49fdc964598a75b91f6d89fae320a71bc63eaa1
Kit <kit.rosenberg@engineering.digital.dwp.gov.uk>
2026-01-30 13:09:26 +0000

    srb journey updates
    GitOrigin-RevId: 9f126de992ebe328c86d0c55dc6c94c5c4dcaf4c

--- app/routes/srbpreaward.js
+++ app/routes/srbpreaward.js
@@ -3,25 +3,19 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 router.post('/srb-pre-award/srb-question', function (req, res) {
   if (req.body['srbQuestion'] === 'yes') {
-    res.redirect('reason-for-srb')
+    res.redirect('payment-date')
   } else {
     res.redirect('dashboard')
   }
 })
 
-router.post('/srb-pre-award/reason-for-srb', function (req, res) {
-  if (req.body['srbReason'] === '3a' || req.body['srbReason'] === '2a') {
-    res.redirect('payment-date')
-  } else {
-    res.redirect('calculate-award')
-  }
-})
-
 router.post('/srb-pre-award/payment-date', function (req, res) {
   if (req.body['paymentDate'] === 'yes') {
-    res.redirect('calculate-award')
-  } else {
+    res.redirect('enter-date')
+  } else if (req.body['paymentDate'] === 'no') {
     res.redirect('send-email')
+  } else {
+    res.redirect('calculate-award')
   }
 })
 
@@ -29,20 +23,54 @@ router.post('/srb-pre-award/send-email', (req, res) => {
   res.redirect('dashboard')
 })
 
+router.post('/srb-pre-award/enter-date', (req, res) => {
+  res.redirect('calculate-award')
+})
+
 router.post('/srb-pre-award/calculate-award', function (req, res) {
   if (req.body['enterSP'] === '230.25') {
     res.redirect('enter-pp')
   } else {
-    res.redirect('enter-date')
+    res.redirect('check-answers')
   }
 })
 
 router.post('/srb-pre-award/enter-pp', (req, res) => {
-  res.redirect('enter-date')
+  res.redirect('check-answers')
 })
 
-router.post('/srb-pre-award/enter-date', (req, res) => {
+router.post('/srb-pre-award/payment-date-2', function (req, res) {
+  if (req.body['paymentDate2'] === 'yes') {
+    res.redirect('enter-date-2')
+  } else if (req.body['paymentDate2'] === 'no') {
+    res.redirect('send-email')
+  } else {
+    res.redirect('calculate-award-2')
+  }
+})
+
+router.post('/srb-pre-award/enter-date-2', (req, res) => {
+  res.redirect('calculate-award-2')
+})
+
+router.post('/srb-pre-award/calculate-award-2', function (req, res) {
+  if (req.body['enterSP2'] === '230.25') {
+    res.redirect('enter-pp-2')
+  } else {
+    res.redirect('check-answers')
+  }
+})
+
+router.post('/srb-pre-award/enter-pp-2', (req, res) => {
   res.redirect('check-answers')
+})
+
+router.post('/srb-pre-award/check-answers', (req, res) => {
+  res.redirect('confirm-award')
+})
+
+router.post('/srb-pre-award/confirm-award', (req, res) => {
+  res.redirect('dashboard')
 })
 
 module.exports = router
