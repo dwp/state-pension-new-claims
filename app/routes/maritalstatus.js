6868665fad2c06582362decb894cc1d1fb9a0c5f
Matt Hall <matt.hall1@engineering.digital.dwp.gov.uk>
2026-04-29 09:58:53 +0100

    Change marital status to widowed
    GitOrigin-RevId: 7426e448efb1a2b15c3db1c7fe3c2cabcb452579

---
+++ app/routes/maritalstatus.js
@@ -0,0 +1,33 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+// Divorced conversion task or new claims task
+
+router.post('/marital-status/widowed/change-marital-status', function (req, res) {
+if (req.body['maritalstatus'] === 'widowed') {
+    res.redirect('late-spouse-dod')
+  } else {
+    res.redirect('divorced')
+  }
+})
+
+router.post('/marital-status/widowed/late-spouse-dod', function (req, res) {
+  res.redirect('partner-sp-in-payment')
+})
+
+router.post('/marital-status/widowed/partner-sp-in-payment', function (req, res) {
+  if (req.body['spousesp'] === 'spinpayment') {
+    res.redirect('check-details-info-missing-new')
+  } else {
+    res.redirect('check-details-info-missing-conversion')
+  }
+})
+
+module.exports = router
