0f4186cea9dff493fb07df59eaa3446d0f2654f3
Martin Underhill <martin.underhill@engineering.digital.dwp.gov.uk>
2026-06-14 17:10:54 +0100

    Contact prefs timeline
    GitOrigin-RevId: 78843a31ae5d1f48d084a727d36ca25d87dad0af

--- app/routes/welsh.js
+++ app/routes/welsh.js
@@ -1,21 +1,49 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+function addTimelineEvent(req, eventData) {
+  req.session.data['timelineHistory'] = req.session.data['timelineHistory'] || []
+
+  req.session.data['timelineHistory'].push(eventData)
+
+  if (req.session.data['timelineHistory'].length > 10) {
+    req.session.data['timelineHistory'].shift()
+  }
+}
+
 router.post('/welsh/contact-pref-spoken', (req, res) => {
   req.session.data['showBanner'] = 'yes'
   req.session.data['lastChanged'] = 'spoken'
+
+  addTimelineEvent(req, {
+    type: 'spoken',
+    value: req.session.data['contactPrefSpoken']
+  })
+
   res.redirect('record-contact')
 })
 
 router.post('/welsh/contact-pref-written', (req, res) => {
   req.session.data['showBanner'] = 'yes'
   req.session.data['lastChanged'] = 'written'
+
+  addTimelineEvent(req, {
+    type: 'written',
+    value: req.session.data['contactPrefWritten']
+  })
+
   res.redirect('record-contact')
 })
 
 router.post('/welsh/contact-pref-alt-format', (req, res) => {
   req.session.data['showBanner'] = 'yes'
   req.session.data['lastChanged'] = 'altFormat'
+
+  addTimelineEvent(req, {
+    type: 'altFormat',
+    value: req.session.data['altFormat']
+  })
+
   res.redirect('record-contact')
 })
 
