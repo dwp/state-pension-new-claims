cf0f424f1f1f914b73796ed29d552f1f26d05eac
Martin Underhill <martin.underhill@engineering.digital.dwp.gov.uk>
2026-07-06 17:44:49 +0100

    Merge branch 'main' into dap-success-tweak
    GitOrigin-RevId: c8af9c39ebcd46cf380e618ee356a2ba23c1cda7

--- app/routes.js
+++ app/routes.js
@@ -83,6 +83,7 @@ const dap = require('./routes/dap')
 const historicpayments = require('./routes/historicpayments')
 const generic = require('./routes/generic')
 const maritalstatus = require('./routes/maritalstatus')
+const nvdob = require('./routes/nvdob')
 
 // copy the router use and update the sprint version
 router.use(newclaims)
@@ -164,5 +165,6 @@ router.use(dap)
 router.use(historicpayments)
 router.use(generic)
 router.use(maritalstatus)
+router.use(nvdob)
 
 module.exports = router
